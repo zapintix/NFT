dacd2c1060070a21cfc390965a5546bf71df8e94
zapintix <fedorovanton33@gmail.com>
2024-08-13 20:20:14 +0300

    update serializers

--- store/serializers.py
+++ store/serializers.py
@@ -1,20 +1,29 @@
-from rest_framework.serializers import ModelSerializer
+from rest_framework import serializers
 from store.models import Nft, Category, TypePrice
 
 
-class CatSerializer(ModelSerializer):
+class CatSerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
 
 
-class NftSerializer(ModelSerializer):
+class NftSerializer(serializers.ModelSerializer):
+    cat = serializers.PrimaryKeyRelatedField(read_only=False, queryset=Category.objects.all())
+    typePrice = serializers.PrimaryKeyRelatedField(read_only=False, queryset=TypePrice.objects.all())
+
     class Meta:
         model = Nft
         fields = '__all__'
 
+    def to_representation(self, instance):
+        ret = super().to_representation(instance)
+        ret['cat'] = instance.cat.name
+        ret['typePrice'] = instance.typePrice.name
+        return ret
 
-class TypePriceSerializer(ModelSerializer):
+
+class TypePriceSerializer(serializers.ModelSerializer):
     class Meta:
-        modul = TypePrice
+        model = TypePrice
         fields = '__all__'
